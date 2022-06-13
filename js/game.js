2423ec8151d08b4a29a1ff9f9ec9b315f9779678
Maik Kempe <maik.kempe@googlemail.com>
2022-06-13 16:30:15 +0200

    first classes

--- js/game.js
+++ js/game.js
@@ -1,7 +1,10 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init() {
     canvas = document.getElementById('canvas');
-    character.src = '../img/1.Sharkie/1.IDLE/1.png'
+    ctx = canvas.getContext('2d');
+    character.src = '../img/1.Sharkie/1.IDLE/1.png';
+    ctx.drawImage(character, 20, 20, 50, 150);
 }
