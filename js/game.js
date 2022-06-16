e5cf8dd9e3477c71832d160761374121089120e4
Maik Kempe <maik.kempe@googlemail.com>
2022-06-16 21:13:54 +0200

    change functions

--- js/game.js
+++ js/game.js
@@ -5,5 +5,5 @@ let world = new World();
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    console.log(world.character);
+    console.log(world.character, world.enemies);
 }
--- models/enemies/pufferfish.class.js
+++ models/enemies/pufferfish.class.js
@@ -1,7 +1,7 @@
 class Pufferfish extends MovealbeObject {
 
     constructor() {
-        super().loadImage('img/1.Sharkie/1.IDLE/1.png');
+        super().loadImage('img/2_enemy/1_pufferfish/1_swim/1.swim1.png');
     }
     
 }
