cdadcf5f2e265bd28f4ab648c7fe65cf31af9144
Maik Kempe <maik.kempe@googlemail.com>
2022-08-02 17:08:28 +0200

    update

--- models/enemies/pufferfish.class.js
+++ models/enemies/pufferfish.class.js
@@ -2,6 +2,7 @@ class Pufferfish extends MovealbeObject {
 
     constructor() {
         super().loadImage('img/2_enemy/1_pufferfish/1_swim/1.swim1.png');
+        this.x = 200 + Math.random() * 400; //zahl zwischen 200 und 800;
+        this.y = Math.random() * 220;
     }
-    
 }
--- models/world.class.js
+++ models/world.class.js
@@ -17,7 +17,9 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //clear canvas
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
-
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+        })
         self = this;
         requestAnimationFrame(function () { //führt draw() solange aus, wie es die Grafikkarte hergibt.
             self.draw();
