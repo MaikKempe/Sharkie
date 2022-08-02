b94f6e89d2c3304f600219255d62fd889dac7427
Maik Kempe <maik.kempe@googlemail.com>
2022-08-02 12:42:17 +0200

    upadte

--- js/game.js
+++ js/game.js
@@ -4,8 +4,5 @@ let world;
 function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas);
-<<<<<<< HEAD
-=======
     console.log(world.character);
->>>>>>> 20d13c73b9c1921801570e7de43e032b5f0dee1a
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,11 +1,6 @@
 class MovealbeObject {
-<<<<<<< HEAD
     x = 0;
     y = 150;
-=======
-    x = 250;
-    y = 400;
->>>>>>> 20d13c73b9c1921801570e7de43e032b5f0dee1a
     img;
     height = 320;
     width = 340;
