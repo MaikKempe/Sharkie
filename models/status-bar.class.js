13dc721cf9c9b9e42a82e435cdeb979a7aa6e794
Maik Kempe <maik.kempe@googlemail.com>
2022-10-27 18:12:58 +0200

    added statusbar

---
+++ models/status-bar.class.js
@@ -0,0 +1,10 @@
+class StatusBar extends DrawableObject {
+    IMAGES_LIFE = [
+        'img/4_marcadores/green/life/0_copia3.png',
+        'img/4_marcadores/green/life/20_copia4.png',
+        'img/4_marcadores/green/life/40_copia3.png',
+        'img/4_marcadores/green/life/60_copia3.png',
+        'img/4_marcadores/green/life/80_copia3.png',
+        'img/4_marcadores/green/life/100_copia2.png'
+    ]
+}
