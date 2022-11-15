581553d28e0c8900645d46e0b0ffc38d99c0d602
Maik Kempe <maik.kempe@googlemail.com>
2022-11-15 17:14:09 +0100

    added coin and poison count function in level class

--- models/level.class.js
+++ models/level.class.js
@@ -6,11 +6,23 @@ class Level {
     levelEndX = 4 * 720;
     startY = -150;
     endY = 210;
+    allCoins;
+    allPoisons;
   
 
     constructor(enemies, collectableObjects, backgroundObjects) {
         this.enemies = enemies;
         this.collectableObjects = collectableObjects;
         this.backgroundObjects = backgroundObjects;
+        this.allCoins = this.countCoins();
+        this.allPoisons = this.countPoisons();
+        console.log(this.allCoins, this.allPoisons);
+    }
+
+    countCoins() {
+        return this.collectableObjects.filter(o => o instanceof Coin).length;
+    }
+    countPoisons() {
+        return this.collectableObjects.filter(o => o instanceof Poison).length;
     }
 }
